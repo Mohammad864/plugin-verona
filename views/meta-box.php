8442eff2291e947e937be872267b5c6aa378edf4
mohammad864 <developer8640@gmail.com>
2024-08-19 12:56:14 +0330

    some bugs fixed

--- views/meta-box.php
+++
@@ -1,5 +0,0 @@
-<?php
-wp_nonce_field('isbn_meta_box', 'isbn_meta_box_nonce');
-$isbn = get_post_meta($post->ID, '_isbn', true);
-?>
-<input type="text" name="isbn" value="<?php echo esc_attr($isbn); ?>" class="widefat">
